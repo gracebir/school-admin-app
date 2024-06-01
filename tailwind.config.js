d1487d6a8ee510c78bc1a0083073df2021b1a30b
gracebir <grace.devolop@gmail.com>
2024-06-01 22:18:26 +0200

    adding the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      },
+      colors: {
+        
+      }
+    },
   },
   plugins: [],
 }
